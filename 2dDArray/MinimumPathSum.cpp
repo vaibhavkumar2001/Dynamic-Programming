d1f3ce2dddd96165ae5ad08404e6308f836dd780
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-05-15 14:13:11 +0530

    Implement bottom-up dynamic programming approach for minimum path sum calculation

--- 2dDArray/MinimumPathSum.cpp
+++ 2dDArray/MinimumPathSum.cpp
@@ -36,6 +36,27 @@ public:
         dp[i][j] = grid[i][j] + min(down, right);
         return dp[i][j];
     }
+
+    //BOTTOM UP APPROACH
+
+    int SolveUsingTab(vector<vector<int>>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+
+    for (int i = m - 1; i >= 0; i--) {
+        for (int j = n - 1; j >= 0; j--) {
+            if (i == m - 1 && j == n - 1) {
+                dp[i][j] = grid[i][j];
+            } else {
+                int down = (i + 1 < m) ? dp[i + 1][j] : INT_MAX;
+                int right = (j + 1 < n) ? dp[i][j + 1] : INT_MAX;
+                dp[i][j] = grid[i][j] + min(down, right);
+            }
+        }
+    }
+    return dp[0][0];
+}
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
